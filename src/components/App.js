81452097af919b6606bb075637c6dfbf9d855575
Daniil Kutyavin <dkutyavin@web3tech.ru>
2019-11-30 21:17:00 +0300

    chore: change class to functional component

--- src/components/App.js
+++ src/components/App.js
@@ -1,66 +1,53 @@
-import React from 'react'
+import React, { useEffect } from 'react'
 import { connect } from 'react-redux'
 import { fetchAllQuotes, getRandomQuote } from '../modules/quotes'
 import { changeBgColor } from '../modules/ui'
 
-class App extends React.PureComponent {
-  componentDidMount = () => {
-    const url =
-      'https://gist.githubusercontent.com/camperbot/5a022b72e96c4c9585c32bf6a75f62d9/raw/e3c6895ce42069f0ee7e991229064f167fe8ccdc/quotes.json'
-    this.props.fetchAllQuotes(url)
-    this.props.changeBgColor()
-  }
+function App(props) {
+  const { loading, fetchAllQuotes, changeBgColor, quote, getRandomQuote, color } = props
+  useEffect(
+    () => {
+      const url =
+        'https://gist.githubusercontent.com/DaniilKutyavin/0d335200fcbbee1fc5337d164e801c1d/raw/e3c6895ce42069f0ee7e991229064f167fe8ccdc/quotes.json'
+      fetchAllQuotes(url)
+      changeBgColor()
+    },
+    [fetchAllQuotes, changeBgColor]
+  )
 
-  componentDidUpdate = prevProps => {
-    if (this.props.loading !== prevProps.loading) {
-      this.props.getRandomQuote()
-    }
-    if (this.props.quote !== prevProps.quote) {
-      this.props.changeBgColor()
-    }
-  }
-
-  tweetClkHandle = () => {
-    const {
-      quote: { quote: text, author },
-    } = this.props
+  const tweetClkHandle = () => {
+    const { quote: text, author } = quote
     const tweet = `"${text}" ${author}`
     window.open(`https://twitter.com/intent/tweet?text=${tweet}`, '_blank')
   }
-  render() {
-    const {
-      quote: { quote: text, author },
-      loading,
-      getRandomQuote,
-    } = this.props
-    return (
-      <div id="app" className={this.props.color}>
-        {loading ? (
-          <h1>Loading...</h1>
-        ) : (
-          <div id="quote-box">
-            <p id="text">
-              &ldquo;
-              {text}
-              &rdquo;
-            </p>
-            <p id="author">{author}</p>
-            <div id="buttons">
-              <button id="tweet-quote" onClick={this.tweetClkHandle}>
-                Tweet quote
-              </button>
-              <button
-                id="new-quote"
-                className={this.props.color}
-                onClick={() => getRandomQuote()}>
-                New quote
-              </button>
-            </div>
-          </div>
-        )}
+  const { quote: text, author } = quote
+
+  if (loading) return <Loading />
+
+  return (
+    <div id="app" className={color}>
+      <div id="quote-box">
+        <p id="text">
+          &ldquo;
+          {text}
+          &rdquo;
+        </p>
+        <p id="author">{author}</p>
+        <div id="buttons">
+          <button id="tweet-quote" onClick={tweetClkHandle}>
+            Tweet quote
+          </button>
+          <button id="new-quote" className={color} onClick={getRandomQuote}>
+            New quote
+          </button>
+        </div>
       </div>
-    )
-  }
+    </div>
+  )
+}
+
+function Loading() {
+  return <h1>Loading...</h1>
 }
 
 const mapStateToProps = state => ({
