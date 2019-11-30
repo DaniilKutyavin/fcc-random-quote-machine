ff36c66d9efa4c3038ba3244a65435c707a2a311
Daniil Kutyavin <dkutyavin@web3tech.ru>
2019-12-01 00:26:54 +0300

    chore: remove redux

--- src/index.js
+++ src/index.js
@@ -1,17 +1,9 @@
 import React from 'react'
 import { render } from 'react-dom'
 import registerServiceWorker from './registerServiceWorker'
-import { Provider } from 'react-redux'
-
-import store from './store'
 
 import './index.scss'
 import App from './components/App'
 
-render(
-  <Provider store={store}>
-    <App />
-  </Provider>,
-  document.getElementById('root')
-)
+render(<App />, document.getElementById('root'))
 registerServiceWorker()
