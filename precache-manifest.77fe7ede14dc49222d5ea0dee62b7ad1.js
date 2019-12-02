d3c58dc26b5f4672422b418a7548b12dd3a3a30e
Daniil Kutyavin <dkutyavin@web3tech.ru>
2019-12-02 15:25:48 +0300

    Updates

---
+++ precache-manifest.77fe7ede14dc49222d5ea0dee62b7ad1.js
@@ -0,0 +1,22 @@
+self.__precacheManifest = (self.__precacheManifest || []).concat([
+  {
+    "revision": "86efa0ea2b59ba6d532976bd4aefbb81",
+    "url": "/fcc-random-quote-machine/index.html"
+  },
+  {
+    "revision": "5d2fb6afe7493fd9b922",
+    "url": "/fcc-random-quote-machine/static/css/main.769e859f.chunk.css"
+  },
+  {
+    "revision": "2098021d0cc2fb9dc743",
+    "url": "/fcc-random-quote-machine/static/js/2.076d0e41.chunk.js"
+  },
+  {
+    "revision": "5d2fb6afe7493fd9b922",
+    "url": "/fcc-random-quote-machine/static/js/main.0dfc29c2.chunk.js"
+  },
+  {
+    "revision": "1bfffb1fa6355b799de1",
+    "url": "/fcc-random-quote-machine/static/js/runtime-main.7ac738c6.js"
+  }
+]);
