9a395c80f5121e52306e79438dc0430c5602308f
Daniil Kutyavin <dkutyavin@web3tech.ru>
2019-12-02 10:14:51 +0300

    change inner divs of loader to ::berfore,after

--- src/components/loader/Loader.js
+++ src/components/loader/Loader.js
@@ -2,10 +2,5 @@ import React from 'react'
 import './Loader.scss'
 
 export default function Loader({ size }) {
-  return (
-    <div className="loader" style={{ height: size, width: size }}>
-      <div className="loader__circle" />
-      <div className="loader__circle loader__circle--delayed" />
-    </div>
-  )
+  return <div className="loader" style={{ height: size, width: size }} />
 }
